878f54d6bbf49273533c3cf53a0fd46b43bb1551
Magicking <s@6120.eu>
2018-03-04 04:00:43 +0100

    Updating tgif to automatically push rush recorded with ttystudio to https://github.com/Magicking/typescript_rushs/branches probably a bug with Index

--- scripts/tgif.sh
+++ scripts/tgif.sh
@@ -1,20 +1,26 @@
 #!/bin/zsh
 setopt nullglob
 
-ARTICLES_GIT_DIR="${HOME}/source/gocode/src/github.com/Magicking/articles"
-RUSH_DIR="${ARTICLES_GIT_DIR}/typescript"
+ARTICLES_REPO="${HOME}/source/gocode/src/github.com/Magicking/articles"
+RUSH_DIR="${HOME}/source/gocode/src/github.com/Magicking/typescript_rushs"
 RUSH_PREFIX="ttystudio_frames_rush_"
 IDX=(${RUSH_DIR}/${RUSH_PREFIX}*)
 if [ x"${1}" != "x" ]; then
   RUSH_NAME="${1}-${IDX}"
+  RUSH_BRANCH="${1}"
 else
   # https://stackoverflow.com/questions/2793812/generate-a-random-filename-in-unix-shell
   RUSH_NAME=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32`
+  RUSH_BRANCH="${RUSH_NAME}"
 fi
 RUSH_FILE="${RUSH_DIR}/${RUSH_PREFIX}${RUSH_NAME}".json
 
-"${ARTICLES_GIT_DIR}"/scripts/ttystudio/bin/ttystudio --record "${RUSH_FILE}" --interval=100
-echo git --git-dir="${ARTICLES_GIT_DIR}/.git/" checkout -b "${RUSH_NAME}"
-echo git --git-dir="${ARTICLES_GIT_DIR}/.git/" add ${RUSH_NAME}
-echo git --git-dir="${ARTICLES_GIT_DIR}/.git/" commit ${RUSH_NAME} -m "Add ttystudio rush json frames for « ${RUSH_NAME} » attempt ${IDX}"
-echo git --git-dir="${ARTICLES_GIT_DIR}/.git/" checkout master
+"${ARTICLES_REPO}"/scripts/ttystudio/bin/ttystudio --record "${RUSH_FILE}" --interval=100
+PWD=`pwd`
+cd "${RUSH_DIR}"
+git checkout -b "${RUSH_PREFIX}${RUSH_BRANCH}"
+git add ${RUSH_FILE}
+git commit ${RUSH_FILE} -m "Add ttystudio rush json frames for « ${RUSH_NAME} » attempt ${IDX}"
+git push --set-upstream origin "${RUSH_PREFIX}${RUSH_BRANCH}"&
+git checkout master
+cd "${PWD}"
