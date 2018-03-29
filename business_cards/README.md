32d61550fe07577c10e8bc0f01a9adf651211729
Magicking <s@6120.eu>
2018-03-29 03:55:46 +0200

    Add README

---
+++ business_cards/README.md
@@ -0,0 +1,12 @@
+# Business card generator
+
+The QRCode point to an https address to redeem Token
+
+This directory contains:
+
+ - SVG Draft
+ - SVG Template
+ - Go svg generator
+
+# How to use
+![Terminal example](https://raw.githubusercontent.com/Magicking/articles/master/business_cards/docs/howto.gif)
--- business_cards/run.sh
+++ business_cards/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #uuoc
-cat "$1" | awk '{ print "go run gen_business.go --key "$2" > bc/business-"$1".svg; convert -density 300 bc/business-"$1".svg bc/business-"$1".png" }' | xargs -P4 -L1 -I{} /bin/sh -c '{}'
+cat "$1" | awk '{ print "./gen_bc --key "$2" > bc/business-"$1".svg; convert -density 300 bc/business-"$1".svg bc/business-"$1".png" }' | xargs -P4 -L1 -I{} /bin/sh -c '{}'
